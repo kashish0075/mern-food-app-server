f84a483f54e28b8bdd87e56f44af9cb925d8c762
kashish0075 <kashish0075.cse19@chitkara.edu.in>
2021-12-02 17:18:16 +0530

    mern-food-app-server

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const seedDb = require('./seed.js');
+const cors = require('cors');
+
+
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Db connected");
+})
+.catch((err) => {
+  console.log(err);
+});
+
+//seedDb();
+
+app.use(express.json());
+app.use(cors(
+  {
+    origin: ['http://localhost:3000',],
+    credentials: true,
+  }
+));
+
+const foodRoutes = require('./api/foodRoutes');
+
+app.get('/hello', function(req,res){
+   res.status(200).json({msg: 'Hello from the server'});
+});
+
+
+app.use(foodRoutes);
+
+
+app.listen(process.env.PORT || 8000, function(){
+    console.log("Server is listen at port");
+});
